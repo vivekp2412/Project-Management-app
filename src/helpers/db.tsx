5ae165b64210d94a8a137d725c66f05f8a9bac0e
vivekp2412 <vivekp2412@gmail.com>
2023-08-28 21:24:08 +0530

    Bug Solved

--- src/helpers/db.tsx
+++ src/helpers/db.tsx
@@ -27,7 +27,7 @@ import { UserValues, Project } from "../Types/types";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_APIKEY,
-  authDomain: import.meta.env.VITE_APP_AUHHDOMAIN,
+  authDomain: import.meta.env.VITE_APP_AUTHDOMAIN,
   projectId: import.meta.env.VITE_APP_PROJECTID,
   storageBucket: import.meta.env.VITE_APP_STORAGEBUCKET,
   messagingSenderId: import.meta.env.VITE_APP_MESSAGINGSENDERID,
